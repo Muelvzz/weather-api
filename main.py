695194b2849473b26fa9f95e144a8ea385d7a970
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-07-15 19:20:52 +0800

    add form and route

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/city", methods=["GET", "POST"])
+def show_weather():
+    if request.method == "POST":
+        city_title = request.form.get("city")
+        return render_template("show_weather.html", city = city_title)
+    else:
+        return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
