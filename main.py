73ae89a2e8f40ba948b767b749bebe4a92a49370
Muelvin Lopez <muelvinlopez25@gmail.com>
2025-07-16 11:07:33 +0800

    add API's

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+import requests
+import json
 
 app = Flask(__name__)
+API_KEY = "84ab1dfa4bef78418d9185402b63e99f"
 
 @app.route("/")
 def home():
@@ -9,8 +12,15 @@ def home():
 @app.route("/city", methods=["GET", "POST"])
 def show_weather():
     if request.method == "POST":
+
         city_title = request.form.get("city")
-        return render_template("show_weather.html", city = city_title)
+        API_CALL = f"https://api.openweathermap.org/data/2.5/weather?q={city_title}&appid={API_KEY}"
+
+        response = requests.get(API_CALL)
+        weather_data = response.json()
+
+        return render_template("show_weather.html", city = city_title, weather_data=weather_data)
+    
     else:
         return redirect(url_for("home"))
 
